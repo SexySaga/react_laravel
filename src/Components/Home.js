ae9efa82bfa163d035ea5a31290e149142d0da9b
SexySaga <imabhimondal@gmail.com>
2024-01-02 17:50:16 +0530

    change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 function Home(){
     return(
         <div>
-            <h1>Home Page</h1>
+            <h1>Home Page</h1><br/>
+            <h2>Home Page ........</h2>
         </div>
     )
 }
