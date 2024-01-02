a5ce6f1355e459731b40fdeb77803b7e03b92e52
SexySaga <imabhimondal@gmail.com>
2024-01-02 16:58:37 +0530

    pages change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Nav from './Components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home';
+import Featuring from './Components/Featuring';
+import Pricing from './Components/Pricing';
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
         <Nav />
         <Routes>
           <Route path='/home' element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/features' element={<Featuring />}></Route>
+        </Routes>
+        <Routes>
+          <Route path='/pricing' element={<Pricing />}></Route>
         </Routes>
       </BrowserRouter>
 
---
+++ src/Components/Featuring.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Featuring(){
+    return(
+        <div>
+            <h1>Featuring Page</h1>
+        </div>
+    )
+}
+export default Featuring;
